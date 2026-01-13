02ab0b151d0b1d08f9ca2a97bd3014405b7ee05e
Abhishek Wake <abhishekwake111@gmail.com>
2026-01-13 21:12:58 +0530

    Revising CSS properties ep7

---
+++ ep7-CSS-Properties/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Temp from "@/components/Temp";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen  max-w-2xl mx-auto flex items-center">
+       <Temp /> 
+    </main>
+   
+  );
+}
