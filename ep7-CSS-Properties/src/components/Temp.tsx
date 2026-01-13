efba4093641d06ba69e29c498837ba459490c17f
Abhishek Wake <abhishekwake111@gmail.com>
2026-01-13 21:31:06 +0530

    learned perspective-distant transform-3d group/ then hover when point on group, cn for merging of different classes,transform rotate-x-23 rotate-z-20 -rotate-y-20 translate-z-20

--- ep7-CSS-Properties/src/components/Temp.tsx
+++ ep7-CSS-Properties/src/components/Temp.tsx
@@ -7,21 +7,26 @@ const Temp = () => {
   className={cn
   ("w-full rounded-2xl bg-neutral-200 min-h-100" , 
     "bg-[radial-gradient(var(--color-neutral-300)_1px,transparent_1px)]",
-    "[background-size:10px_10px]",
-    "p-8 flex justify-center items-center"
+    "[background-size:10px_10px]", 
+    "p-8 flex justify-center items-center group"
   )}
   >
     <div className={cn(
         "size-60 bg-neutral-200 rounded-lg border border-neutral-300",
         "bg-[radial-gradient(var(--color-neutral-300)_1px,transparent_1px)]",
         "[background-size:10px_10px] ",
-        "shadow-2xl relative"
+        "shadow-2xl relative perspective-distant transform-3d",
+        "group/lethal"
     )}
     >
        <img 
        src="https://images.unsplash.com/photo-1750535135451-7c20e24b60c1?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-       alt=""
-       className='h-full w-full object-cover' />
+       alt="" 
+       className={cn(
+        'transition-transform duration-200',
+        'h-full w-full object-cover rounded-2xl group-hover:scale-0',
+        'transform rotate-x-23 rotate-z-20 -rotate-y-20 translate-z-20  '
+       )}  />
     </div>
   </div>
  );
